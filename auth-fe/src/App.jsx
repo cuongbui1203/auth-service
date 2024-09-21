1cb3e974c65215fed2fddfe64deebc7ba2c95405
Kuon <cuongbuimanh1203@gmail.com>
2024-09-21 08:42:49 +0700

    init

---
+++ auth-fe/src/App.jsx
@@ -0,0 +1,26 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import './App.css'
+import { useEffect, useState } from 'react'
+import Home from './Page/Home';
+import Login from './Page/Login';
+
+function App() {
+  const [loggedIn, setLoggedIn] = useState(false)
+  const [email, setEmail] = useState('')
+
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={<Home email={email} loggedIn={loggedIn} setLoggedIn={setLoggedIn} />}
+          />
+          <Route path="/login" element={<Login setLoggedIn={setLoggedIn} setEmail={setEmail} />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
